8885f60f5595eaafeede6f4f8e75f58ad134a1e2
Douglas Almeida <douglas.alu.lmb@gmail.com>
2020-08-25 20:05:37 -0300

    Adding content

--- src/App.js
+++ src/App.js
@@ -1,28 +1,60 @@
-import React from "react";
-
+import React, {useState, useEffect} from "react";
+import api from './services/api';
 import "./styles.css";
 
 function App() {
+
+  const [repositories, setRepositores] = useState([]);
+  const [title, setTitle] = useState('');
+  const [url, setUrl] = useState('');
+  const [techs, setTech ] = useState([]);
+
+  useEffect(() => {
+    async function getDatas(){
+      const response = await api.get('/repositories');
+      setRepositores(response.data);
+    }
+    getDatas();
+  }, [])
+
   async function handleAddRepository() {
-    // TODO
+
+    const response = await api.post('/repositories', {
+        title: 'test front end',
+        url: 'https://github.com/DglsAlmeida/gostack-conceitos-nodejs',
+        techs : [
+          'ReactJS',
+          'NodeJS'
+        ]
+    });
+
+    const repository = response.data;
+  
+    setRepositores([
+      ...repositories,
+      repository
+    ]);
   }
 
   async function handleRemoveRepository(id) {
-    // TODO
+    await api.delete(`/repositories/${id}`);
+    const repo = repositories.filter(repository => repository.id !== id);
+    setRepositores(repo);
   }
 
   return (
     <div>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
-
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
-          </button>
-        </li>
+        {repositories.map(repository => (
+          <li key={repository.id}>
+            {repository.title}
+            <button onClick={() => handleRemoveRepository(repository.id)}>
+              Remover
+            </button>
+          </li>
+        ))}
       </ul>
-
+    
       <button onClick={handleAddRepository}>Adicionar</button>
     </div>
   );
